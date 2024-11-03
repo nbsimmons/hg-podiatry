8cf7f0e330ce1ee4958b0725857f350d2eeb4beb
Nathan Simmons <developer@nbsimmons.com>
2024-11-03 12:44:41 +0000

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+        '*.html',
+        './layout/*.liquid',
+        './sections/*.liquid',
+        './snippets/*.liquid',
+        './assets/*.js'
+    ]
+}
